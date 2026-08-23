0b0061dca85d337d15fab8c21de16d4f6fc556a7
cota <109924735+cota4106@users.noreply.github.com>
2026-08-24 03:09:50 +0900

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,14 @@ const { EnvironmentPlugin, DefinePlugin } = require("webpack");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = (env, argv) => ({
   entry: {
     bundle: "./src/megamoji",
+    // gifworkerは `new Worker("./gifworker.js")` という固定文字列で読み込まれるため、
+    // ファイル名にcontenthashを付けず固定名のままにする(bundleとは別扱い)
     gifworker: "./src/gifworker",
   },
   devServer: {
@@ -19,7 +24,9 @@ module.exports = (env, argv) => ({
   },
   output: {
     path: `${__dirname}/dist`,
-    filename: "[name].js",
+    filename: (pathData) => (
+      pathData.chunk.name === "gifworker" ? "gifworker.js" : "[name].[contenthash].js"
+    ),
     assetModuleFilename: "assets/[hash][ext][query]",
   },
   devtool: "source-map",
@@ -64,11 +71,23 @@ module.exports = (env, argv) => ({
       CODE_VERSION: "",
     }),
     new VueLoaderPlugin(),
+    // index.htmlは静的コピーではなく、ビルドのたびに<script>タグを
+    // (ハッシュ付きの)正しいファイル名で自動生成する
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/static/index.html`,
+      filename: "index.html",
+      chunks: ["bundle"], // gifworkerは<script>タグとして埋め込まない(Workerとして動的読み込みのため)
+      inject: "body",
+    }),
     new CopyWebpackPlugin({
       patterns: [{
         context: `${__dirname}/static`,
         from: "*",
         to: `${__dirname}/dist`,
+        globOptions: {
+          // index.htmlはHtmlWebpackPluginが生成するので、単純コピー対象からは除外する
+          ignore: ["**/index.html"],
+        },
       }],
     }),
   ],
